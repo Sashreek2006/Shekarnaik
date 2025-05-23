c40d941a2b70daf731cb878e357448f3bdf4c19e
Sashreek2006 <148124546+Sashreek2006@users.noreply.github.com>
2025-05-23 16:36:13 +0530

    CODE1.py

--- untitled6.py
+++ untitled6.py
@@ -1,20 +1,11 @@
 import re
 from google.colab import files
-
-# Upload the text file
 uploaded = files.upload()
-
-# Assuming only one file was uploaded
 for filename in uploaded.keys():
   print(f'User uploaded file "{filename}"')
   with open(filename, 'r') as f:
     content = f.read()
-
-  # Find all numbers in the content using regex
   numbers = re.findall(r'\d+', content)
-
-  # Convert the found numbers to integers and sum them
   total_sum = sum(int(number) for number in numbers)
-
   print(f"The sum of all numbers in the file is: {total_sum}")
 
